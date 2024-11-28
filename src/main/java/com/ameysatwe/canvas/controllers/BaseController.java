0bca4dcf5f0a6b4bc6f366fa1e494c5d4c60762f
Amey Satwe <amey10051999@gmail.com>
2024-11-27 21:54:22 -0500

    Basic controller added, MVP tested for final project

---
+++ src/main/java/com/ameysatwe/canvas/controllers/BaseController.java
@@ -0,0 +1,28 @@
+package com.ameysatwe.canvas.controllers;
+
+import com.ameysatwe.canvas.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class BaseController {
+
+    @GetMapping("/")
+    public String getMapping(){
+        return "index";
+    }
+
+    @GetMapping("/home")
+    public ModelAndView GetMapping(User user, ModelMap modelMap){
+        user.setEmail("asatwe@gmail.com");
+//        modelMap.addAttribute(user);
+//        return "hello-world";
+        ModelAndView modelAndView = new ModelAndView("hello-world");
+        modelAndView.addObject("user",user);
+        return modelAndView;
+    }
+}
